eed0bd7a3f932e615faea2395e75309d7335d008
Pavel Yampolsky <pavel.yampolsky@grandparade.co.uk>
2019-05-10 00:08:21 +0200

    apply format

--- features/step_definitions/open_page_step.rb
+++ features/step_definitions/open_page_step.rb
@@ -2,22 +2,10 @@ require_relative '../pages/travelpayouts_page.rb'
 
 
 Given("user opens travelpayouts page") do
-
-
   @travelpayouts_page = Travelpayouts_page.new(@browser)
-
-
   @travelpayouts_page.open_travelpayouts_page
-
-
 end
 
-
-
-
 Then(/^user closes travelpayouts page$/) do
-
   @travelpayouts_page.close
-
-
 end
